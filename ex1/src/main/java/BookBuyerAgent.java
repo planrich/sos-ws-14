36c8f32cf0d7d57b4d4cbe1533047cf6fa57db90
Richard Plangger <rich@pasra.at>
2014-11-24 15:51:35 +0100

    init

---
+++ ex1/src/main/java/BookBuyerAgent.java
@@ -0,0 +1,8 @@
+
+import jade.core.Agent;
+public class BookBuyerAgent extends Agent {
+    protected void setup() {
+        // Printout a welcome me
+        System.out.print("hello agent" + getAID().toString());
+    }
+}
