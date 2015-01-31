7ccfb50bff61a981bb6a3a16fc23a8f30ae03f71
Rene Koller <r.koller@me.com>
2015-01-31 11:25:46 +0100

    Added iris visualizations

---
+++ ex3-protocol/export-iris.sh
@@ -0,0 +1,36 @@
+cd ../ex3
+PDIR=../ex3-protocol/img
+
+function e() {
+    VIS=$1
+    ID=$2
+
+    bash som.sh iris-hex iris "--vis $VIS -e $PDIR/iris-$ID-hex.png"
+    bash som.sh iris iris "--vis $VIS -e $PDIR/iris-$ID.png "
+}
+
+function ep() {
+    VIS=$1
+    ID=$2
+    PARAM=$3
+
+    bash som.sh iris-hex iris "--vis $VIS --visParams $PARAM -e $PDIR/cl-$ID-hex.png"
+    bash som.sh iris iris "--vis $VIS --visParams $PARAM -e $PDIR/cl-$ID.png"
+}
+
+e 'ActivityHistogram' 'activity'
+e 'HitHistogramColour' 'hit'
+e 'UMatrix' 'umatrix'
+e 'DMatrix' 'dmatrix'
+e 'PMatrix' 'pmatrix'
+e 'QuantizationErr' 'qe'
+e "FuzzyColouring" 'fuzzy-colouring'
+e 'MeanQuantizationErr' 'mqe'
+e 'TopographicError4Units' 'topo-4'
+e 'DistortionSqrt' 'dist'
+ep 'NeighbourhoodKnn' 'nh-knn-1' '1'
+ep 'NeighbourhoodKnn' 'nh-knn-2' '2'
+ep 'NeighbourhoodKnn' 'nh-knn-3' '3'
+ep 'NeighbourhoodRadius' 'nh-radius-0,1' '0.1'
+ep 'NeighbourhoodRadius' 'nh-radius-0,2' '0.2'
+ep 'NeighbourhoodRadius' 'nh-radius-0,3' '0.3'
